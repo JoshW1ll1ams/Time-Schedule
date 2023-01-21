cf139f89d0e8a9d2a910651c0008675490f90c27
JoshW1ll1ams <117525232+JoshW1ll1ams@users.noreply.github.com>
2023-01-21 01:24:50 +0000

    Added local storage functionality

--- Assets/JS/logic.js
+++ Assets/JS/logic.js
@@ -1,38 +1,67 @@
+$(document).ready(function() {
 var MainContainer = $("#container");
 
 var today = moment()
 
-var CurrentHour = today.format("H")
-console.log("hello")
- // console.log((moment(i, ["HH"]).format("H")));
+var CurrentHour = parseInt(today.format("H"))
+
+
 for(i = 9; i< 18; i++)
 {
-    var BoxDiv = $("<div>")
+    var timeI = parseInt((moment(i, ["HH"]).format("H")));
 
+    var BoxDiv = $("<div>");
 
-    var TimeDiv = $("<div>")
-    TimeDiv.addClass("hour")
-    
-    var TimeDivText = $("<p>")
-    TimeDivText.text(moment(i, ["HH"]).format("h A"))
-    TimeDiv.append(TimeDivText)
+    var TimeDiv = $("<div>");
+    TimeDiv.addClass("hour");
+    var TimeDivText = $("<p>");
 
+    TimeDivText.text(moment(i, ["HH"]).format("h A"));
+    TimeDiv.append(TimeDivText);
 
-    var InputBox= $("<input>")
-    InputBox.addClass("past")
+    var InputBox= $("<input>");
+    InputBox.val(localStorage.getItem("ButtonClick"+(i-9)))
 
-    var SubmitButton= $("<button>")
-    SubmitButton.addClass("saveBtn")
+    if(timeI < CurrentHour)
+    {
+        InputBox.addClass("past inputbox");
+    }
+    if(timeI == CurrentHour)
+    {
+        InputBox.addClass("present inputbox");
+    }
+    if(timeI > CurrentHour)
+    {
+        InputBox.addClass("future inputbox");
+    }
 
+    $(InputBox).attr("inputnum",i-9);
 
-  
-    BoxDiv.append(TimeDiv)
-    BoxDiv.append(InputBox)
-    BoxDiv.append(SubmitButton)
+    var SubmitButton= $("<button>");
+    SubmitButton.addClass("saveBtn");
+    $(SubmitButton).attr("buttonnum",i-9);
 
-    BoxDiv.addClass("row")
-    MainContainer.append(BoxDiv)
+    BoxDiv.append(TimeDiv);
+    BoxDiv.append(InputBox);
+    BoxDiv.append(SubmitButton);
 
-    
+    BoxDiv.addClass("row");
+    MainContainer.append(BoxDiv);
 }
 
+
+InputBox.val(localStorage.getItem("ButtonClick"+i-1))
+
+
+$('.saveBtn').click(function(event){
+    var ButtonPressed = this.getAttribute('buttonnum');
+    var RelativeInput =  $('#container').children().eq(ButtonPressed).children().eq(1).val();
+    //savedTasks.ButtonPressed = RelativeInput;
+   // console.log(savedTasks.ButtonPressed)
+    localStorage.setItem("ButtonClick"+ButtonPressed,RelativeInput)
+});
+//localStorage.setItem("ButtonClick"+0,RelativeInput)
+ var saved = localStorage.getItem("ButtonClick"+0)
+ console.log(saved)
+
+});
